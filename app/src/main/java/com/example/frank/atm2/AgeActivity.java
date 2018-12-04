0350b27950b0a1a26b5ee6aa2190da81dff38775
purplesunset871019 <frank871019>
2018-12-04 16:38:40 +0800

    AgeAdapter

--- app/src/main/java/com/example/frank/atm2/AgeActivity.java
+++ app/src/main/java/com/example/frank/atm2/AgeActivity.java
@@ -1,18 +1,30 @@
 package com.example.frank.atm2;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgeActivity extends BaseActivity {
 
+    int[] numbers = {19,20,21,22,23,24,25};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //TODO: set adpter
     }
 
     public void next(View view){
@@ -25,5 +37,38 @@ public class AgeActivity extends BaseActivity {
 
     public void back(View view){
         finish();
+    }
+
+    class AgeAdapter extends RecyclerView.Adapter<AgeAdapter.AgeHolder>{
+
+        @NonNull
+        @Override
+        public AgeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.age_row,parent,false);
+
+            return new AgeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AgeHolder holder, int position) {
+            holder.ageText.setText(numbers[position]+"");
+            if(numbers[position] == 19){
+                holder.ageText.setTextColor(Color.RED);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return numbers.length;
+        }
+
+        class AgeHolder extends RecyclerView.ViewHolder{
+            TextView ageText;
+            public AgeHolder(View itemView){
+                super (itemView);
+                ageText = itemView.findViewById(R.id.tv_age);
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/frank/atm2/MainActivity.java
+++ app/src/main/java/com/example/frank/atm2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.frank.atm2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,6 +20,7 @@ import java.util.List;
 public class MainActivity extends BaseActivity {
     private static final int RC_LOGIN = 100;
     boolean login = false;
+    List<String> fruits = Arrays.asList("芭樂","香蕉","蘋果");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +36,44 @@ public class MainActivity extends BaseActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter();
+        recyclerView.setAdapter(new FruitAdapter());
 
 
         //listview();
     }
 
-    class Fruitadpter extends
+    class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder> {
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            View view = LayoutInflater.from(context)
+                    .inflate(android.R.layout.simple_list_item_1,parent,false);
+            FruitViewHolder fruitViewHolder = new FruitViewHolder(view);
+            return fruitViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder holder, int position) {
+            holder.fruitname.setText(fruits.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+        }
+
+        class FruitViewHolder extends RecyclerView.ViewHolder{
+            TextView fruitname;
+            public FruitViewHolder(View itemView) {
+                super(itemView);
+                fruitname = itemView.findViewById(android.R.id.text1);
+            }
+        }
+    }
 
 
     private void listview() {
-        List<String> fruits = Arrays.asList("芭樂","香蕉","蘋果");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
@@ -57,7 +90,7 @@ public class MainActivity extends BaseActivity {
             else{
                 login = true;
 
-                if(user.isValid()){
+                if(!user.isValid()){
                     Intent nick = new Intent(this,NicknameActivity.class);
                     startActivity(nick);
                 }
